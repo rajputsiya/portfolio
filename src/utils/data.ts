203def60c9da8369044cf8d1c94f9f9300df783c
1rajputshashank@gmail.com <hariomshashank@gmail.com>
2025-02-20 20:30:10 +0530

    initial commit

---
+++ src/utils/data.ts
@@ -0,0 +1,10 @@
+export const data = {
+    "name" : "Siya Rajput",
+    "projects_link" : "https://.vercel.app",
+    "leetcode_link": "https://leetcode.com/",
+    "linkedin" : "https://linkedin.com/in/",
+    "github" : "https://github.com/rajputsiya",
+    "project_repo" : "https://github.com/rajputsiya/details",
+    "about_me_data_1" : "Hi, I’m Siya Rajput, currently pursuing a Bachelor's degree in Computer Science and Engineering.",
+    "about_me_data_2" : "",
+}
