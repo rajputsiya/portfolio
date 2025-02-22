c8049ec0017fa8d239e5e93cea66951b8aceae52
rajputsiya <siyarajput9258@gmail.com>
2025-02-22 12:04:39 +0530

    update links to profile

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,10 +1,10 @@
 export const data = {
     "name" : "Siya Rajput",
     "projects_link" : "https://.vercel.app",
-    "leetcode_link": "https://leetcode.com/",
+    "leetcode_link": "https://leetcode.com/siyarajput",
     "linkedin" : "https://linkedin.com/in/",
     "github" : "https://github.com/rajputsiya",
-    "project_repo" : "https://github.com/rajputsiya/details",
+    "project_repo" : "https://github.com/rajputsiya/portfolio",
     "about_me_data_1" : "Hi, I’m Siya Rajput, currently pursuing a Bachelor's degree in Computer Science and Engineering.",
     "about_me_data_2" : "",
 }
